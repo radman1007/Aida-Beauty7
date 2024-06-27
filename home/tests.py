f872863230f6560a7328bed8a306d071a43df2a4
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:27:48 +0330

    add reverse and one test

--- home/tests.py
+++ home/tests.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class HomePageTests(TestCase):
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
