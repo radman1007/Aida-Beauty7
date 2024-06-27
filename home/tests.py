8aca6f7d89142022193bce5fbf8d3208fec92671
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:36:42 +0330

    write test 4 and check it for home app

--- home/tests.py
+++ home/tests.py
@@ -14,3 +14,7 @@ class HomePageTests(TestCase):
     def test_home_page_url(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        
+    def test_home_page_template_use(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
