64c268fffcea2fb15a33c155aadcf3c10cbf47ca
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:10:05 +0330

    create Product class in product app

--- product/models.py
+++ product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True,blank=True)
+    price = models.PositiveIntegerField(default=0)
+    body = models.TextField()
+    weight = models.PositiveIntegerField()
