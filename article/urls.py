79402e3e3d376d9d7e4660a2b01e5f4a0c13bff6
radman montazer <radman@radmans-MacBook-Pro.local>
2024-06-27 16:06:24 +0330

    create urls.py for article and write path for pages

---
+++ article/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('blog/', views.blog, name="blog"),
+    path('blog-filter/', views.blog_filter, name="blog_filter"),
+    path('blog-detail/', views.blog_detail, name="blog_detail"),
+]
