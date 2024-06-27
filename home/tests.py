a8e8eeab1a6dca553afd12f96aa66b6211e5d2dd
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:29:01 +0330

    debug test.py

--- home/tests.py
+++ home/tests.py
@@ -4,5 +4,5 @@ from django.urls import reverse
 
 class HomePageTests(TestCase):
     def test_home_page_url_by_name(self):
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
