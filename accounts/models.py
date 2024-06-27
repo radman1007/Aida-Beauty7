7d4b53924705d45fef2446ae1d23cbb350519f6d
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:32:32 +0330

    debug test.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=12 , unique=True , verbose_name='Phone Number')
+# class CustomUser(AbstractUser):
+#     phone = models.CharField(max_length=12 , unique=True , verbose_name='Phone Number')
