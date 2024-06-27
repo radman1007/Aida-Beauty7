4506069a8276876e61337c2c5433a4cf9e6ff9f7
radman montazer <radman@radmans-MacBook-Pro.local>
2024-06-27 15:12:47 +0330

    add url paths for pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('compare/', views.compare, name='compare'),
+    path('faq/', views.faq, name='faq'),
 ]
