22c2884d3126823dfa1b9968fd1c464880b7aed3
radman montazer <radman@radmans-MacBook-Pro.local>
2024-06-27 15:54:23 +0330

    include product urls

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('product/', include('product.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
