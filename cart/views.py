9fea0412a0d02602066c1332800a0f7f6ef807e2
radman montazer <radman@radmans-MacBook-Pro.local>
2024-06-27 16:42:30 +0330

    render cart pages

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cart(request):
+    return render(request, "cart.html")
+
+def checkout(request):
+    return render(request, "checkout.html")
