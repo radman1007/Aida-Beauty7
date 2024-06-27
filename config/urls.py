921b168ac354dfe7474ff0a4ee32db6369c9ca2f
radman montazer <radman@radmans-MacBook-Pro.local>
2024-06-27 15:37:01 +0330

    include accounts path

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,6 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
