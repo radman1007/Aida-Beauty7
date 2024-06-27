d6d558bf64ee21a6f69de52ed32df7b74d742fb8
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:35:56 +0330

    write test 3 and check it

--- home/tests.py
+++ home/tests.py
@@ -10,3 +10,7 @@ class HomePageTests(TestCase):
     def test_home_page_content(self):
         response = self.client.get(reverse('index'))
         self.assertContains(response, 'خانه')
+        
+    def test_home_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
