544369bb4bdc4888ecba9afda695c4f99dac1210
radman1007 <radman.cod110@gmail.com>
2024-06-27 19:20:54 +0330

    add phone number to customuser

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    phone = models.CharField(max_length=12 , unique=True , verbose_name='Phone Number')
