4a9e55f18ebfcc0324c5c3f3dfb6166bf6b3349e
radman1007 <radman.cod110@gmail.com>
2024-07-15 22:47:59 +0330

    debug

--- product/models.py
+++ product/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# class Product(models.Model):
-#     title = models.CharField(max_length=255)
-#     image = models.ImageField(null=True,blank=True)
-#     price = models.PositiveIntegerField(default=0)
-#     body = models.TextField()
-#     weight = models.PositiveIntegerField()
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True,blank=True)
+    price = models.PositiveIntegerField(default=0)
+    body = models.TextField()
+    weight = models.PositiveIntegerField()
+    mater = models.CharField(max_length=255)
