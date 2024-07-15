cc60096f0eb3bc7b3be1745f86e9154f426b2889
radman1007 <radman.cod110@gmail.com>
2024-07-15 19:09:49 +0330

    debug

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import profile
 
 urlpatterns = [
     path('profile/', profile, name='profile')
--- config/settings.py
+++ config/settings.py
@@ -106,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
