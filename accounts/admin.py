ec17450849737a7f119b8b7c9df82396b591026e
radman1007 <radman.cod110@gmail.com>
2024-07-15 18:41:43 +0330

    test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email', 'phone', 'username')
--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=12, unique=True, verbose_name='Phone Number')
+    phone = models.CharField(max_length=12, verbose_name='Phone Number')
