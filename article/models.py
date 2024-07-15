b9eaade4d0a1f360d7cdbd399b153f6d447b9a84
radman1007 <radman.cod110@gmail.com>
2024-07-15 21:16:36 +0330

    test

--- article/models.py
+++ article/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.title
 
@@ -12,7 +12,7 @@ class Blog(models.Model):
     image = models.ImageField(null=True,blank=True)
     author = models.CharField(max_length=250)
     body = models.TextField()
-    category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category, related_name='category')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
