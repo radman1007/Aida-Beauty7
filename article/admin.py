a34644faeb59fecfe890a0b0dc18f9ff7cfca267
radman1007 <radman.cod110@gmail.com>
2024-07-16 01:55:47 +0330

    add static blog admin

--- article/admin.py
+++ article/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Blog, Category
+from .models import Blog, Category, StaticBlog
 
 admin.site.register(Category)
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created')
+
+
+@admin.register(StaticBlog)
+class StaticBlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created')
