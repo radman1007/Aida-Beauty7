3573250943e6d9af1b883d3e34760295be23cacf
radman1007 <radman.cod110@gmail.com>
2024-07-15 19:28:15 +0330

    add blogadmin

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Blog, Category
 
-# Register your models here.
+admin.site.register(Category)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created')
