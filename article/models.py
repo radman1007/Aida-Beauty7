710e580b58ab271319edb77a1c524ce31096b6e8
radman1007 <radman.cod110@gmail.com>
2024-07-15 18:28:52 +0330

    add blog class

--- article/models.py
+++ article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
