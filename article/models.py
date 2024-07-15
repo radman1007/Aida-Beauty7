93d9c0f8f4ad2f2af5ba8e441cd80ecc69241830
radman1007 <radman.cod110@gmail.com>
2024-07-15 18:34:57 +0330

    add category for blog

--- article/models.py
+++ article/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
     body = models.TextField()
+    category = models.ManyToManyField(Category)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
