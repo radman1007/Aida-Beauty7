d376ac90690a3d6501db1c104d5b59569b808007
radman1007 <radman.cod110@gmail.com>
2024-07-16 01:54:30 +0330

    add new class

--- article/models.py
+++ article/models.py
@@ -22,3 +22,14 @@ class Blog(models.Model):
     
     def get_absolute_url(self):
         return reverse("blog_detail", args={self.id})
+    
+    
+class StaticBlog(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(null=True,blank=True)
+    author = models.CharField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
