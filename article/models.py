f18e601ace6b51c031cac343d9eeab3b23cce1ce
radman1007 <radman.cod110@gmail.com>
2024-07-15 18:30:19 +0330

    debug

--- article/models.py
+++ article/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
-    created = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
