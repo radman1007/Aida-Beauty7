99718593edfb09631d652cdb668b5fcfded600e7
radman1007 <radman.cod110@gmail.com>
2024-07-16 02:39:22 +0330

    add contact

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    text = models.TextField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
