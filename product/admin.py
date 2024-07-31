66ed75b3817f9979d2c499a54e818d2c232c4ad8
radman1007 <radman.cod110@gmail.com>
2024-07-31 21:04:38 +0330

    debug

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'created')
     ordering = ('-created',)
+    
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'message', 'star')
