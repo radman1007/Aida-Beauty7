1c894e68b2aadb017d3e3c0aeb9094c5033811c4
radman1007 <radman.cod110@gmail.com>
2024-07-31 22:12:39 +0330

    add and check the save form

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Comment
 from article.models import Category
+from .forms import CommentForm
 
 def product(request):
     posts = Product.objects.all()
@@ -12,6 +13,12 @@ def product(request):
 def product_detail(request, pk):
     post = get_object_or_404(Product, pk=pk)
     comments = post.comments.filter(publish=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.product = post
+            new_comment.save()
     context = {
         'post' : post,
         'comments' : comments,
