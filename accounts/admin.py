dcf97f7a5b2a1b370f160abbce11ca3b22b635f9
radman1007 <radman.cod110@gmail.com>
2024-08-04 20:03:19 +0330

    debug

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,16 +3,13 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .forms import UserChangeForm, UserCreationForm
 from .models import User
+from product.models import Address
 
 
 class UserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
     form = UserChangeForm
     add_form = UserCreationForm
 
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
     list_display = ["phone", "fullname", "is_admin"]
     list_filter = ["is_admin"]
     fieldsets = [
@@ -20,8 +17,7 @@ class UserAdmin(BaseUserAdmin):
         ("اطلاعات شخصی", {"fields": ["fullname"]}),
         ("دسترسی ها", {"fields": ["is_admin"]}),
     ]
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
+    
     add_fieldsets = [
         (
             None,
@@ -36,8 +32,6 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = []
 
 
-# Now register the new UserAdmin...
 admin.site.register(User, UserAdmin)
-# ... and, since we're not using Django's built-in permissions,
-# unregister the Group model from admin.
+
 admin.site.unregister(Group)
