26710aeabb27d968d510d4b8f008b98305d9be8a
radman1007 <radman.cod110@gmail.com>
2024-08-07 19:36:29 +0330

    change website LANGUAGE

--- config/settings.py
+++ config/settings.py
@@ -119,12 +119,14 @@ AUTHENTICATION_BACKENDS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
