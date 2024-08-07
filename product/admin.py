a4f9da93fe1397ff97673b6b40ceefa017301e36
radman1007 <radman.cod110@gmail.com>
2024-08-08 01:44:30 +0330

    add CommentsInline

--- product/admin.py
+++ product/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 from .models import Product, Comment
 from jalali_date.admin import ModelAdminJalaliMixin
 
+
+class CommentsInline(admin.StackedInline):
+    model = Comment
+    fields = ['name', 'message', 'star', 'email', 'publish']
+    
+
 @admin.register(Product)
 class ProductAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     list_display = ('title', 'price', 'available', 'created')
     ordering = ('-created',)
+    inlines = [
+        CommentsInline,
+    ]
     
     
 @admin.register(Comment)
