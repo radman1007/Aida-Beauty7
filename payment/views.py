39b84f9ac1292ae4702e7cae96bad4ab179fecff
radman1007 <radman.cod110@gmail.com>
2024-11-04 23:54:06 +0330

    test and debug base category a tag in index page

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from cart.models import Order
+import requests
 
 
 def payment_process(request):
     order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
+    
     toman_total_price = order.get_total_price()
     rial_total_price = toman_total_price * 10
     
+# https://payment.zarinpal.com/pg/v4/payment/request.json
